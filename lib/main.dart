19763e7e1d074920e07adc25ba122b88ee05c995
bob_the_builder <milindshandilya899@gmail.com>
2023-12-28 10:40:55 +0000

    final

--- lib/main.dart
+++ lib/main.dart
@@ -7,21 +7,7 @@ void main() {
         body: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
-              colors: [
-                Color(0xf41b004e),
-                Color(0xff52085c),
-                Color(0xf41b004e),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text("Hello, World",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 28,
-                )),
+              
           ),
         ),
       ),
